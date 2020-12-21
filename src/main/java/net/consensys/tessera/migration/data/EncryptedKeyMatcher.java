561f2b5199c1964bd06068bcb3fdae6de1f46899
Peter <peter.rd.fox@gmail.com>
2020-12-21 11:46:51 +0000

    Factor out case where we are not the sender.

--- src/main/java/net/consensys/tessera/migration/data/EncryptedKeyMatcher.java
+++ src/main/java/net/consensys/tessera/migration/data/EncryptedKeyMatcher.java
@@ -22,90 +22,46 @@ public class EncryptedKeyMatcher {
     }
 
     public List<PublicKey> match(final EncryptedPayload transaction, final PrivacyGroupPayload privacyGroup, final boolean weAreSender) {
+        if (!weAreSender) {
+            return handleWhenNotSender(transaction, Arrays.asList(privacyGroup.addresses()));
+        }
+
         List<PublicKey> recipientKeys = new ArrayList<>();
 
-        PublicKey senderKey = Optional.of(transaction.sender())
-                .map(Box.PublicKey::bytesArray)
-                .map(PublicKey::from).get();
+        //we are the sender of this tx, so we have all of the encrypted master keys
+        //the keys listed in the privacy group should be in the same order as they are
+        //used for the EncryptedKey, so just iterate over them to test it
 
-        if (weAreSender) {
-            //we are the sender of this tx, so we have all of the encrypted master keys
-            //the keys listed in the privacy group should be in the same order as they are
-            //used for the EncryptedKey, so just iterate over them to test it
+        PrivateKey privateKey = orionKeyHelper.getKeyPairs()
+                .stream()
+                .filter(kp -> kp.publicKey().equals(transaction.sender()))
+                .findFirst()
+                .map(Box.KeyPair::secretKey)
+                .map(Box.SecretKey::bytesArray)
+                .map(PrivateKey::from)
+                .orElseThrow(() -> new IllegalStateException("local sender key not found"));
 
-            PrivateKey privateKey = orionKeyHelper.getKeyPairs()
-                    .stream()
-                    .filter(kp -> kp.publicKey().equals(transaction.sender()))
-                    .findFirst()
-                    .map(Box.KeyPair::secretKey)
-                    .map(Box.SecretKey::bytesArray)
-                    .map(PrivateKey::from)
-                    .orElseThrow(() -> new IllegalStateException("local sender key not found"));
+        for (int i = 0; i < transaction.encryptedKeys().length; i++) {
+            EncryptedKey encryptedKey = transaction.encryptedKeys()[i];
 
-            for (int i = 0; i < transaction.encryptedKeys().length; i++) {
-                EncryptedKey encryptedKey = transaction.encryptedKeys()[i];
+            for (String possibleRecipientPublicKey : privacyGroup.addresses()) {
+                PublicKey recipientKey = PublicKey.from(Base64.getDecoder().decode(possibleRecipientPublicKey));
 
-                for (String possibleRecipientPublicKey : privacyGroup.addresses()) {
-                    PublicKey recipientKey = PublicKey.from(Base64.getDecoder().decode(possibleRecipientPublicKey));
+                final boolean canDecrypt = canDecrypt(transaction, encryptedKey, recipientKey, privateKey);
 
-                    final boolean canDecrypt = canDecrypt(transaction, encryptedKey, recipientKey, privateKey);
+                if (canDecrypt) {
+                    //hasn't blown up, so must be a success
+                    recipientKeys.add(recipientKey);
 
-                    if (canDecrypt) {
-                        //hasn't blown up, so must be a success
-                        recipientKeys.add(recipientKey);
-
-                        // Found the correct key, no need to keep trying others
-                        break;
-                    }
-                }
-
-                //check we actually found a relevant key
-                if (recipientKeys.size() != (i + 1)) {
-                    //TODO: make a proper error
-                    throw new RuntimeException("could not find a local recipient key to decrypt the payload with");
+                    // Found the correct key, no need to keep trying others
+                    break;
                 }
             }
-        } else {
 
-            // Find the intersection of the privacy groups public keys and our local keys
-            // as those are the only keys that are relevant for us now
-            final List<String> ourPossibleRecipientKeys = new ArrayList<>(Arrays.asList(privacyGroup.addresses()));
-            final List<String> ourPublicKeysBase64 = orionKeyHelper.getKeyPairs().stream()
-                    .map(Box.KeyPair::publicKey)
-                    .map(Box.PublicKey::bytesArray)
-                    .map(pkBytes -> Base64.getEncoder().encodeToString(pkBytes))
-                    .collect(Collectors.toList());
-            ourPossibleRecipientKeys.removeIf(k -> !ourPublicKeysBase64.contains(k));
-
-            for (int i = 0; i < transaction.encryptedKeys().length; i++) {
-                EncryptedKey encryptedKey = transaction.encryptedKeys()[i];
-
-                // Try each of the keys to see which one actually
-                for (String ourPublicRecipientKey : ourPossibleRecipientKeys) {
-
-                    Box.KeyPair keypairUnderTest = orionKeyHelper.getKeyPairs().stream()
-                            .filter(kp -> Objects.equals(Base64.getEncoder().encodeToString(kp.publicKey().bytesArray()), ourPublicRecipientKey))
-                            .findFirst()
-                            .get();
-                    PublicKey ourPublicKey = PublicKey.from(keypairUnderTest.publicKey().bytesArray());
-                    PrivateKey ourPrivateKey = PrivateKey.from(keypairUnderTest.secretKey().bytesArray());
-
-                    final boolean canDecrypt = canDecrypt(transaction, encryptedKey, senderKey, ourPrivateKey);
-
-                    if (canDecrypt) {
-                        //hasn't blown up, so must be a success
-                        recipientKeys.add(ourPublicKey);
-
-                        // Found the correct key, no need to keep trying others
-                        break;
-                    }
-                }
-
-                //check we actually found a relevant key
-                if (recipientKeys.size() != (i + 1)) {
-                    //TODO: make a proper error
-                    throw new RuntimeException("could not find a local recipient key to decrypt the payload with");
-                }
+            //check we actually found a relevant key
+            if (recipientKeys.size() != (i + 1)) {
+                //TODO: make a proper error
+                throw new RuntimeException("could not find a local recipient key to decrypt the payload with");
             }
         }
 
@@ -116,84 +72,90 @@ public class EncryptedKeyMatcher {
                                  final QueryPrivacyGroupPayload privacyGroup,
                                  final boolean weAreSender) {
 
+        if (!weAreSender) {
+            return handleWhenNotSender(transaction, Arrays.asList(privacyGroup.addresses()));
+        }
+
         List<PublicKey> recipientKeys = new ArrayList<>();
 
-        PublicKey senderKey = Optional.of(transaction.sender())
-                .map(Box.PublicKey::bytesArray)
-                .map(PublicKey::from).get();
+        //we are the sender of this tx, so we have all of the encrypted master keys
+        //the keys listed in the privacy group should be in the same order as they are
+        //used for the EncryptedKey, so just iterate over them to test it
+        for (int i = 0; i < transaction.encryptedKeys().length; i++) {
+            EncryptedKey encryptedKey = transaction.encryptedKeys()[i];
 
-        if (weAreSender) {
-            //we are the sender of this tx, so we have all of the encrypted master keys
-            //the keys listed in the privacy group should be in the same order as they are
-            //used for the EncryptedKey, so just iterate over them to test it
-            for (int i = 0; i < transaction.encryptedKeys().length; i++) {
-                EncryptedKey encryptedKey = transaction.encryptedKeys()[i];
+            String recipientKeyB64 = privacyGroup.addresses()[i];
+            PublicKey recipientKey = PublicKey.from(Base64.getDecoder().decode(recipientKeyB64));
 
-                String recipientKeyB64 = privacyGroup.addresses()[i];
-                PublicKey recipientKey = PublicKey.from(Base64.getDecoder().decode(recipientKeyB64));
+            PrivateKey privateKey = orionKeyHelper.getKeyPairs()
+                    .stream()
+                    .filter(kp -> kp.publicKey().equals(transaction.sender()))
+                    .findFirst()
+                    .map(Box.KeyPair::secretKey)
+                    .map(Box.SecretKey::bytesArray)
+                    .map(PrivateKey::from)
+                    .orElseThrow(() -> new IllegalStateException("local sender key not found"));
 
-                PrivateKey privateKey = orionKeyHelper.getKeyPairs()
-                        .stream()
-                        .filter(kp -> kp.publicKey().equals(transaction.sender()))
-                        .findFirst()
-                        .map(Box.KeyPair::secretKey)
-                        .map(Box.SecretKey::bytesArray)
-                        .map(PrivateKey::from)
-                        .orElseThrow(() -> new IllegalStateException("local sender key not found"));
+            final boolean canDecrypt = canDecrypt(transaction, encryptedKey, recipientKey, privateKey);
 
-                final boolean canDecrypt = canDecrypt(transaction, encryptedKey, recipientKey, privateKey);
-
-                if (!canDecrypt) {
-                    throw new IllegalStateException("tx we sent cannot be decrypted using private key");
-                }
-
-                //hasn't blown up, so must be a success
-                recipientKeys.add(recipientKey);
+            if (!canDecrypt) {
+                throw new IllegalStateException("tx we sent cannot be decrypted using private key");
             }
-        } else {
 
-            // Find the intersection of the privacy groups public keys and our local keys
-            // as those are the only keys that are relevant for us now
-            final List<String> ourPossibleRecipientKeys = new ArrayList<>(Arrays.asList(privacyGroup.addresses()));
-            final List<String> ourPublicKeysBase64 = orionKeyHelper.getKeyPairs().stream()
-                    .map(Box.KeyPair::publicKey)
-                    .map(Box.PublicKey::bytesArray)
-                    .map(pkBytes -> Base64.getEncoder().encodeToString(pkBytes))
-                    .collect(Collectors.toList());
-            ourPossibleRecipientKeys.removeIf(k -> !ourPublicKeysBase64.contains(k));
+            //hasn't blown up, so must be a success
+            recipientKeys.add(recipientKey);
+        }
 
-            // TODO: maybe find out if the keys we are going to test are in order
-            // TODO: but maybe it doesn't really matter and just brute force it
+        return recipientKeys;
+    }
 
-            for (int i = 0; i < transaction.encryptedKeys().length; i++) {
-                EncryptedKey encryptedKey = transaction.encryptedKeys()[i];
+    private List<PublicKey> handleWhenNotSender(final EncryptedPayload transaction,
+                                                final List<String> privacyGroupAddresses) {
+        List<PublicKey> recipientKeys = new ArrayList<>();
 
-                // Try each of the keys to see which one actually
-                for (String ourPublicRecipientKey : ourPossibleRecipientKeys) {
+        final PublicKey senderKey = Optional.of(transaction.sender())
+                .map(Box.PublicKey::bytesArray)
+                .map(PublicKey::from)
+                .get();
 
-                    Box.KeyPair keypairUnderTest = orionKeyHelper.getKeyPairs().stream()
-                            .filter(kp -> Objects.equals(Base64.getEncoder().encodeToString(kp.publicKey().bytesArray()), ourPublicRecipientKey))
-                            .findFirst()
-                            .get();
-                    PublicKey ourPublicKey = PublicKey.from(keypairUnderTest.publicKey().bytesArray());
-                    PrivateKey ourPrivateKey = PrivateKey.from(keypairUnderTest.secretKey().bytesArray());
+        // Find the intersection of the privacy groups public keys and our local keys
+        // as those are the only keys that are relevant for us now
+        final List<String> ourPossibleRecipientKeys = new ArrayList<>(privacyGroupAddresses);
+        final List<String> ourPublicKeysBase64 = orionKeyHelper.getKeyPairs().stream()
+                .map(Box.KeyPair::publicKey)
+                .map(Box.PublicKey::bytesArray)
+                .map(pkBytes -> Base64.getEncoder().encodeToString(pkBytes))
+                .collect(Collectors.toList());
+        ourPossibleRecipientKeys.removeIf(k -> !ourPublicKeysBase64.contains(k));
 
-                    final boolean canDecrypt = canDecrypt(transaction, encryptedKey, senderKey, ourPrivateKey);
+        for (int i = 0; i < transaction.encryptedKeys().length; i++) {
+            EncryptedKey encryptedKey = transaction.encryptedKeys()[i];
 
-                    if (canDecrypt) {
-                        //hasn't blown up, so must be a success
-                        recipientKeys.add(ourPublicKey);
+            // Try each of the keys to see which one actually
+            for (String ourPublicRecipientKey : ourPossibleRecipientKeys) {
 
-                        // Found the correct key, no need to keep trying others
-                        break;
-                    }
+                Box.KeyPair keypairUnderTest = orionKeyHelper.getKeyPairs().stream()
+                        .filter(kp -> Objects.equals(Base64.getEncoder().encodeToString(kp.publicKey().bytesArray()), ourPublicRecipientKey))
+                        .findFirst()
+                        .get();
+                PublicKey ourPublicKey = PublicKey.from(keypairUnderTest.publicKey().bytesArray());
+                PrivateKey ourPrivateKey = PrivateKey.from(keypairUnderTest.secretKey().bytesArray());
+
+                final boolean canDecrypt = canDecrypt(transaction, encryptedKey, senderKey, ourPrivateKey);
+
+                if (canDecrypt) {
+                    //hasn't blown up, so must be a success
+                    recipientKeys.add(ourPublicKey);
+
+                    // Found the correct key, no need to keep trying others
+                    break;
                 }
+            }
 
-                //check we actually found a relevant key
-                if (recipientKeys.size() != (i + 1)) {
-                    //TODO: make a proper error
-                    throw new RuntimeException("could not find a local recipient key to decrypt the payload with");
-                }
+            //check we actually found a relevant key
+            if (recipientKeys.size() != (i + 1)) {
+                //TODO: make a proper error
+                throw new RuntimeException("could not find a local recipient key to decrypt the payload with");
             }
         }
 
@@ -201,9 +163,9 @@ public class EncryptedKeyMatcher {
     }
 
     private boolean canDecrypt(final EncryptedPayload transaction,
-                              final EncryptedKey encryptedKey,
-                              final PublicKey publicKey,
-                              final PrivateKey ourPrivateKey) {
+                               final EncryptedKey encryptedKey,
+                               final PublicKey publicKey,
+                               final PrivateKey ourPrivateKey) {
 
         // "publicKey" may either be the recipient public key (if we were the sender),
         // or the tx sender public key (if we were a recipient)
